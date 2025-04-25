398fda5a4067fc9cc6c7a328d261f3501b19f017
Ian-Bright <ianbrighton1@gmail.com>
2025-04-25 11:02:23 -0600

    Feat: Aztec scan links added for contracts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,7 @@ export const USDC_TOKEN = {
     decimals: 6
 }
 
+export const AZTEC_SCAN_CONTRACT_URL = 'https://aztecscan.xyz/contracts/instances'
 export const AZTEC_WALLET_LS_KEY = 'aztec_wallet';
 export const OPENBANKING_ESCROW_LS_KEY = 'openbanking_escrow_contract';
 export const OPENBANKING_USDC_LS_KEY = 'openbanking_usdc_contract';
